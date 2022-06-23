0ad9fdf0abf6c8c57a32b6e81c16de19611d00ab
samiksha-159k <80770697+samiksha-159k@users.noreply.github.com>
2022-06-23 13:34:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 
 import './App.css';
-import Account from './Components/Account';
 import Chart from './Components/chart';
-
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
 function App() {
